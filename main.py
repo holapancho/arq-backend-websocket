f948596898bd4930346ff661d381670acf4db994
Francisco Rojas <francisco.rojas.morales@gmail.com>
2023-05-17 21:01:44 -0400

    retorna posiciones "reales"

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import logging
 import asyncio
 from fastapi import FastAPI, WebSocket
-from random import seed, randint
+import random
+from random import seed
 
 seed(1)
 
@@ -11,6 +12,10 @@ app = FastAPI()
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
-        await asyncio.sleep(1.1)
-        payload = randint(0, 10)
-        await websocket.send_json(payload)
+        await asyncio.sleep(2.5)
+        
+        #puntos de interes de limache
+        latitud = round(random.uniform(-33.032, -32.98), 6)
+        longitud = round(random.uniform(-71.27, -71.22), 6)
+        lat_lon_string = { 'latitud':"{:.6f}".format(latitud), 'longitud':"{:.6f}".format(longitud)}
+        await websocket.send_json(lat_lon_string)
